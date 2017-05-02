e362a89d7d7287ba914b025a85dd9647444b2889
I-am Konstantin <Konstantin@I-ams-MacBook-Pro.local>
2017-05-02 13:43:37 -0400

    adding remove head and tail

--- doubly_linkedlist.js
+++ doubly_linkedlist.js
@@ -33,7 +33,7 @@ DoublyLinkedList.prototype.addToHead = function(element) {
     // Handle case for when linked list is not empty
     if (this.head) { 
         //update old heads previous to point to new node
-        this.head.prev = newNode;
+        this.head.previous = newNode;
         //update new nodes next to point to old head
         newNode.next = this.head;
         //set the head pointer to be the new node
@@ -58,7 +58,7 @@ DoublyLinkedList.prototype.addToTail = function(element) {
         //if there is already a tail, change the pointers to point to new node
         //if update the currently existing tails next to point to new node
         this.tail.next = newNode;
-        newNode.prev = this.tail;
+        newNode.previous = this.tail;
         //then finally update the linked list tail to be the new node
         //this.tail = newNode;
     } else {
@@ -72,3 +72,64 @@ DoublyLinkedList.prototype.addToTail = function(element) {
     //increment count
     this._length++;
 };
+
+DoublyLinkedList.prototype.removeHead = function() {
+    //if there is a head, then there is a node or possibly nodes in the list
+    var headExists = this.head,
+        value = null;
+
+    if(headExists) {
+        //save the current value of the head
+        value = this.head.element;
+        //case 1: there are multiple nodes
+        if(this.head.next != null) { 
+            var oldHead = this.head;
+            // set the new head
+            this.head = oldHead.next;
+            //update the prev of the new head to point to null
+            this.head.previous = null;
+            //set the oldHead (previous) to null. garbage collection
+            oldHead = null;
+        } else { //this.head.next is a null, means there is only one node
+            this.head = null;
+            this.tail = null;
+        }
+        //since headExists
+        this._length--;
+    } else {
+        //there is no head OR !this.head... So linked list is empty
+        //return value; // no need to return null here, since that is the default value
+    }
+    return value;
+};
+
+DoublyLinkedList.prototype.removeTail = function() {
+
+    var tailExists = this.tail,
+        value = null;
+
+    if(tailExists) {
+        //save the current value of the tail
+        value = this.tail.element;
+        //check whether there is only one tail or multiple
+        if(this.tail.previous !== null) {
+            //in this case there are multiple nodes. update pointers
+            var oldTail = this.tail;
+            //update the tail to be the old tails previous node
+            this.tail = oldTail.previous;
+            //set the next of the tail to null
+            this.tail.next = null;
+        } else {
+            //there is only one node in the collection
+            this.head = null;
+            this.tail = null;
+        }
+        //since tail Exists, decrement the collection count
+        this._length--;
+    } else {
+        //return value;
+    }
+    //return the name of tail removed to called (we would be nice to know what was removed)
+    return value;
+};
+
