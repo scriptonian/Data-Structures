ae4ea0e5d4fd1cbc8dbfede9f0d9d0cdeeb856a7
I-am Konstantin <Konstantin@i-ams-mbp.home>
2017-06-24 20:23:41 -0400

    Adding Stack class for ES5 and ES2015

---
+++ stack-es6.js
@@ -0,0 +1,48 @@
+class Stack {
+    constructor() {
+        this.items = [];
+    }
+
+    //Stack Operations Go Here
+    push(element) {
+        //add item to the top of the stack
+        this.items.push(element);
+    }
+
+    pop() {
+        //remove item at the top of the stack
+        this.items.pop();
+    }
+
+    peek() {
+        //return the item at top of the stack
+        let lastItem = this.items.length-1;
+        return this.items[lastItem];
+    }
+
+    isEmpty() {
+        //return boolean whether stack is empty
+        return this.items.length === 0;
+    }
+
+    size() {
+        //return total number of items in stack
+        return this.ltems.length;
+    }
+
+    clear() {
+        //remove all items from the stack
+        this.items = [];
+    }
+}
+
+//Using the Stack
+let stack = new Stack();
+stack.push("Data 1");
+stack.push("Data 2");
+stack.push("Data 3");
+console.log(stack);
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack);
+console.log(stack.size());
---
+++ stack.js
@@ -0,0 +1,45 @@
+function Stack() {
+    //store stack items in an array
+    this.items = [];
+}
+
+Stack.prototype = {
+    //Stack Operations Go Here
+
+    //add item to the top of the stack
+    push : function(element){
+        this.items.push(element);
+    },
+    //remove item at the top of the stack
+    pop : function() {
+        this.items.pop();
+    },
+    //return the item at top of the stack
+    peek : function() {
+        var lastItem = this.items.length-1;
+        return this.items[lastItem];
+    },
+    //return boolean whether stack is empty
+    isEmpty : function(){
+        return this.items.length === 0;
+    },
+    //return total number of items in stack
+    size : function(){
+        return this.items.length;
+    },
+    //remove all items from the stack
+    clear : function(){
+        this.items = [];
+    }
+};
+
+//Using the Stack
+var stack = new Stack();
+stack.push("Data 1");
+stack.push("Data 2");
+stack.push("Data 3");
+console.log(stack);
+console.log(stack.peek());
+console.log(stack.pop());
+console.log(stack);
+console.log(stack.size());
