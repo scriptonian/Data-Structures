c97ed20dda2712c0df3e5d7c6beb452487203b5a
I-am Konstantin <Konstantin@I-ams-MacBook-Pro.local>
2017-06-28 15:06:21 -0400

    add queue classes for es5 and es2015

---
+++ queue-es6.js
@@ -0,0 +1,38 @@
+class Queue {
+  
+    //constructor function
+    constructor() {
+        this.items = [];
+    }
+    
+    //Stack Operations Go Here
+    enqueue(element) {
+        this.items.push(element);
+    }
+    
+    //remove item from the queue
+    dequeue() {
+        this.items.shift();
+    }
+    
+    //return item at the front of the queue
+    front() {
+        return this.items[0];
+    }
+    
+    //return item at the back of the queue
+    back() {
+        var last = this.items.length - 1;
+        return this.items[last];
+    }
+    
+    //display all the items in queue
+    toString() {
+        return this.items.toString();
+    }
+    
+    //empty the queue
+    empty() {
+        this.items = [];
+    }
+}
---
+++ queue.js
@@ -0,0 +1,39 @@
+function Queue() {
+  //store queue items in an array
+  this.items = [];
+}
+
+Queue.prototype = {
+  //Queue Operations Go Here
+  
+  //add item to the top of the queue
+  enqueue : function(element) {
+      this.items.push(element);
+  },
+  
+  //remove item from the queue
+  dequeue : function() {
+      this.items.shift();
+  },
+  
+  //return item at the front of the queue
+  front : function() {
+      return this.items[0];
+  },
+  
+  //return item at the back of the queue
+  back : function() {
+      var last = this.items.length - 1;
+      return this.items[last];
+  },
+  
+  //display all the items in queue
+  toString : function() {
+      return this.items.toString();
+  },
+  
+  //empty the queue
+  empty : function() {
+      this.items = [];
+  }
+};
