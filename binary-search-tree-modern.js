0e7e0618d0222ceba93ae170d23992ca9ccbff87
I-am Konstantin <Konstantin@i-ams-mbp.home>
2017-09-05 23:50:06 -0400

    adding BST insert for ES5 and ES2015 and beyond

--- binary-search-tree-modern.js
+++ binary-search-tree-modern.js
@@ -8,19 +8,55 @@ class TreeNode {
 
 class BinarySearchTree {
     constructor() {
-        let root = null;
+        this.root = null;
+        this.count = 0;
     }
 
-    toString() {
-        return this.keyValue;
+    insert(key) {
+        //create a new tree
+        let newNode = new TreeNode(key);
+        //if root is not empty set the new node to be the root, else add recursively
+        this.root === null ? this.root = newNode : this.insertTo(this.root, newNode);
+        //increment counter
+        this.count++;
     }
 
-    insert(key) {}
+    insertTo(positionNode, newNode){
+        // First Scenario: if the new nodes value is less than the current node. Move in the left direction of positionNode
+        if(newNode.keyValue < positionNode.keyValue) {
+            //if the left node is empty, then make this the new left node
+            if(positionNode.left === null) {
+                positionNode.left = newNode;
+            } else {
+                //add it to the left node
+                this.insertTo(positionNode.left, newNode);
+            }
+        } else { // Second Scenario: if the new nodes value is greater than the current node. Move in the right direction of positionNode
+            if(positionNode.right === null) {
+                positionNode.right = newNode;
+            } else {
+                //add it to the right node
+                this.insertTo(positionNode.right, newNode);
+            }
+        }
+    }
+    
     find(key) {}
     remove(key) {}
     max(){}
     min() {}
     traverse(){}
+    treeCount() {
+        return counter;
+    }
+    toString() {
+        return this.keyValue;
+    }
 }
 
-    
+var bst = new BinarySearchTree();
+bst.insert(60);
+bst.insert(90);
+bst.insert(30);
+bst.insert(70);
+console.log(bst);
--- binary-search-tree.js
+++ binary-search-tree.js
@@ -8,15 +8,52 @@ function TreeNode(keyValue) {
 }
 
 function BinarySearchTree() {
-    var root = null;
+    this.root = null;
+    //not private but you can refactor if you want
+    this.count = 0;
 }
 
 BinarySearchTree.prototype = {
-    insert: function(key) {},
+    insert: function(key) {
+        //create a new tree
+        var newNode = new TreeNode(key);
+        //if root is not empty set the new node to be the root, else add recursively
+        this.root === null ? this.root = newNode : this.insertTo(this.root, newNode);
+        //increment counter
+        this.count++;
+    },
+    insertTo: function(positionNode, newNode){
+        // First Scenario: if the new nodes value is less than the current node. Move in the left direction of positionNode
+        if(newNode.keyValue < positionNode.keyValue) {
+            //if the left node is empty, then make this the new left node
+            if(positionNode.left === null) {
+                positionNode.left = newNode;
+            } else {
+                //add it to the left node
+                this.insertTo(positionNode.left, newNode);
+            }
+        } else { // Second Scenario: if the new nodes value is greater than the current node. Move in the right direction of positionNode
+            if(positionNode.right === null) {
+                positionNode.right = newNode;
+            } else {
+                //add it to the right node
+                this.insertTo(positionNode.right, newNode);
+            }
+        }
+    },
     find: function(key) {},
     remove: function(key) {},
     max: function(){},
     min: function() {},
-    traverse: function(){}
+    traverse: function(){},
+    treeCount: function() {
+        return this.count;
+    }
 }
 
+var bst = new BinarySearchTree();
+bst.insert(60);
+bst.insert(90);
+bst.insert(30);
+bst.insert(70);
+console.log(bst);
