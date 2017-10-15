59ede40cabb776ba44e52277dcbdaf07c4d7a5b5
I-am Konstantin <Konstantin@i-ams-mbp.home>
2017-10-15 13:06:53 -0400

    new and improved shortest path method

--- graph-es5.js
+++ graph-es5.js
@@ -38,17 +38,25 @@ Graph.prototype = {
     breathFirstSearch: function(startingVertex) {
         var color = [],
             distances = [],
-            edgeTo = [],          
+            preceding = [],          
             queue = new Queue(),
             isEmptyQueue = true,
             //create an array of object
             setVertexColorsAndEdges = function() {
                 for(var i = 0; i < this.vertices.length; i++) {
                     color[this.vertices[i]] = 'white';
-                    distances[this.vertices[i]] = 0;
-                    edgeTo[this.vertices[i]] = null;
+                    distances[this.vertices[i]] = -1;
+                    //we initialize all preceding distance to null with the exception
+                    //of the current start vertex.
+                    if(this.vertices[i] === startingVertex) {
+                        distances[this.vertices[i]] = 0;
+                        preceding[this.vertices[i]] = startingVertex;
+                    } else {
+                        preceding[this.vertices[i]] = null;
+                    }                    
                 }
             };
+        
         //set colors and distances on all vertices
         setVertexColorsAndEdges();
         //enqueue vertex onto queue
@@ -65,10 +73,16 @@ Graph.prototype = {
                 if(color[adjVertex] === 'white') {
                     //if color is white change to grey because we have now discovered it
                     color[adjVertex] = 'grey';
-                    //increment count on the vertex weight
-                    distances[adjVertex] = distances[queueFrontVertex] + 1;
-                    //record the path that lead from one vertex to another vertex
-                    edgeTo[adjVertex] = queueFrontVertex;
+                    //since we initialized distances with -1, at the top we need to reset that
+                    //to 1 and start counting properly. else we increase by 1
+                    if(distances[queueFrontVertex] === -1) {
+                        distances[queueFrontVertex] = 1;
+                    } else {
+                        //increment the edge distance count
+                        distances[adjVertex] = distances[queueFrontVertex] + 1;
+                    }
+                    //record the path that lead from one vertex to another vertex. The preceding node
+                    preceding[adjVertex] = queueFrontVertex;
                     //next add this to the queue
                     queue.enqueue(adjVertex);
                 }
@@ -83,74 +97,66 @@ Graph.prototype = {
 
         return {
             distances : distances,
-            edgeTo : edgeTo
+            preceding : preceding
         };   
     },
 
     vertexExist: function(v) {
-        return this.vertices.indexOf(v) === 0;
+        return this.vertices.indexOf(v) >= 0;
     },
 
-    shortestPathToAll: function(fromVertex, bfs) {
-        if(!this.vertexExist(fromVertex)) {
+    shortestPathToAll: function(source, bfs) {
+        //check to see if vertex exists
+        if(!this.vertexExist(source)) {
             return console.log('Starting Vertex not found');
         }
-
+        
+        //since we already know how to output shortest path from source to destination
+        //lets reuse our shortest path method
         for(var i = 0; i < this.vertices.length; i++) {
             var currentVertex = this.vertices[i];
-            var finalPath = new Stack();
-            for(var j = currentVertex; j !== fromVertex; j = bfs.edgeTo[j]) {
-                finalPath.push(j);
-            }
-            //finally push the starting vertex to stack
-            finalPath.push(fromVertex);
-            var vStrings = finalPath.pop();
-            while(!finalPath.isEmpty()) {
-                vStrings += ' - ' + finalPath.pop();
-            }
-            console.log(vStrings);
-        }
+            this.shortest_path(bfs, source, currentVertex);
+        } 
     },
 
-    shortestPathFromTo: function(fromVertex, toVertex) {
-        var queue = new Queue();
-        queue.enqueue(fromVertex);
-        var visited = [];
-        visited[fromVertex] = true;
-        var paths = [];
-
-        while(!queue.isEmpty()) {
-            //get vertex in the front of queue
-            var queueFrontVertex = queue.dequeue();
-            var edges = this.getAdjacencyListVertex(queueFrontVertex);
-            //console.log(edges);
-            for(var i = 0; i < edges.length; i++) {
-                var currentEdge = edges[i];
-                if(!visited[currentEdge]) {
-                    //mark them as visited
-                    visited[currentEdge] = true;
-                    queue.enqueue(currentEdge);
-                    paths[currentEdge] = queueFrontVertex;
-                    //console.log(paths[edges[currentEdge]]);
-                }
-            }
+    shortest_path: function(bfs, source, destination) {
+        //get the distance table array values
+        var distances = bfs.distances,
+            precedingNode = bfs.preceding,
+            finalString,
+            //use a stack to store the backtracking path
+            path = new Stack();
+        
+        //the first to be added to the stack is a destination. however if source and destination
+        //are the same, then there is no need to add the destination. If you dont see this, you will
+        //see K-K or M-M in your output if K or M is your source. If source and destination are the same
+        //simply just present K or M 
+        if(!(source === destination)) {
+            path.push(destination);
+        } 
+        
+        //find the last preceding node in order to get to the source
+        var previousVertex = precedingNode[destination];
+        //console.log(previousVertext);
+        
+        //Perform backtracking
+        while (previousVertex !== null && previousVertex !== source) {
+            path.push(previousVertex);
+            previousVertex = precedingNode[previousVertex];
         }
         
-        if(!visited[toVertex]) {
-            return undefined;
+        if(previousVertex === null) {
+            console.log('There is not path from ' + source + ' to ' + destination);
+        } else { // the previous is equal to the source.
+            //add the source
+            path.push(source);
         }
-
-        var finalPath = [];
-       
-        for(var j = toVertex; j !== fromVertex; j = paths[j]) {
-            finalPath.push(j);
+        //print final results
+        finalString = path.pop();
+        while(!path.isEmpty()) {
+            finalString += ' - ' + path.pop();
         }
-        
-        //finally push the starting vertex to stack
-        finalPath.push(fromVertex);
-
-        var pathString = finalPath.reverse().join('-');
-        return pathString;
+        console.log(finalString);
     },
 
     toString: function() {
@@ -201,8 +207,9 @@ graph.addEdge('M', 'F');
 //graph.toString();
 //graph.breathFirstSearch('K');
 
-var startVertex = vertices[0];
-var bfs = graph.breathFirstSearch(startVertex);
-graph.shortestPathToAll(startVertex, bfs);
-//var shortestPathFromTo = graph.shortestPathFromTo('T', 'F');
-//console.log("Path String: ", shortestPathFromTo);
+var source = vertices[0];
+var destination = vertices[5];
+var bfs = graph.breathFirstSearch(source);
+//console.log(bfs);
+//graph.shortest_path(bfs, source, destination);
+graph.shortestPathToAll(source, bfs);
